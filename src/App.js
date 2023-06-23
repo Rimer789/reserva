a937cef749fdea06a4a747eb4d93147073b6516d
rimer rosa herrera <201809665@est.umss.edu>
2023-06-22 23:29:43 -0400

    pues vamos ahi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import "./App.css";
-import Card from "./components/Card";
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
+
+const Declaracion = () => {
+  const [isAccepted, setIsAccepted] = useState(null);
+
+  const handleYesClick = () => {
+    window.open('https://wa.me/59167566295?text=si%20iremos :) %20', '_blank');
+    setIsAccepted(true);
+  };
+
+  const handleNoMouseOver = () => {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    const noBtn = document.querySelector('#noBtn');
+    noBtn.style.setProperty('top', `${randomY}%`);
+    noBtn.style.setProperty('left', `${randomX}%`);
+    noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+  };
+
+  
+
   return (
-    <div className="home-page">
-      <h1 className="titulo-llamativo">Reserva tu turno con</h1>
-      <Card />
+    <div className="contenedor">
+      <br />
+      <br />
+      <p>si vamos ir al cine nove? -_- </p>
+      
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <button id="yesBtn" onClick={handleYesClick}>
+        Si
+      </button>
+      <button id="noBtn" onMouseOver={handleNoMouseOver}>
+        No
+      </button>
+      {isAccepted !== null && (
+        <p>Tu respuesta fue: {isAccepted ? 'Sí' : 'No'}</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default Declaracion;
